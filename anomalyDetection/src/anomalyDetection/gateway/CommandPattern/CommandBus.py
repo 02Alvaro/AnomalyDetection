d9baa3b9c93416841a3985e8e56f26411760c317
Álvaro <90500200+02Alvaro@users.noreply.github.com>
2023-11-02 00:00:46 +0100

    feat: :zap: implemented dae algorithm fully operative

--- anomalyDetection/src/anomalyDetection/gateway/CommandPattern/CommandBus.py
+++ anomalyDetection/src/anomalyDetection/gateway/CommandPattern/CommandBus.py
@@ -1,15 +1,26 @@
-from anomalyDetection.gateway.CommandPattern.Command import Command, CommandHandler
+from anomalyDetection.gateway.Algorithms.dae.DaeCommand import DaeCommand
+from anomalyDetection.gateway.Algorithms.dae.DaeCommandHandler import DaeCommandHandler
+from anomalyDetection.gateway.Algorithms.LstmVae.LstmVaeCommand import LstmVaeCommand
+from anomalyDetection.gateway.Algorithms.LstmVae.LstmVaeCommandHandler import (
+    LstmVaeCommandHandler,
+)
+from anomalyDetection.gateway.CommandPattern.Command import Command
+from anomalyDetection.gateway.CommandPattern.CommandHandler import CommandHandler
 
 
 class CommandBus:
     def __init__(self):
         self.handlers = {}
+        self.register_handler(LstmVaeCommand, LstmVaeCommandHandler())
+        self.register_handler(DaeCommand, DaeCommandHandler())
 
-    def register(self, command_type, handler: CommandHandler):
+    def register_handler(self, command_type, handler: CommandHandler):
         self.handlers[command_type] = handler
 
     def execute(self, command: Command):
         handler = self.handlers.get(type(command))
         if handler is None:
-            raise Exception(f"No handler found for {type(command).__name__}")
+            raise Exception(
+                f"No handler found for command type {type(command).__name__}"
+            )
         handler.execute(command)
