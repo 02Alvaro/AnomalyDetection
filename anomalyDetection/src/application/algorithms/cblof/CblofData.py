294eb6c120e5a370136fb52cd18254f67f0ed239
Álvaro <90500200+02Alvaro@users.noreply.github.com>
2023-11-10 01:56:51 +0100

    feat: :tada: some pyod algorithms implemented

---
+++ anomalyDetection/src/application/algorithms/cblof/CblofData.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from application.factories.AlgorithmDataFactory import AlgorithmDataFactory
+from domain.interfaces.AlgorithmData import AlgorithmData
+
+
+@dataclass
+@AlgorithmDataFactory.dataClass_for("cblof")
+class CblofData(AlgorithmData):
+    n_clusters: int = 8
+    contamination: float = 0.1
+    clustering_estimator = None
+    alpha: float = 0.9
+    beta: float = 5
+    use_weights: bool = False
+    check_estimator: bool = False
+    random_state: int = None
+    tarjet_variable: str = None
