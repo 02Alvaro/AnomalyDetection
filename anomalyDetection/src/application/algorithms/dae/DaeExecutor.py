cbf7d767ab07e8814d73fdf814a9ac4b0723dbe7
Álvaro <90500200+02Alvaro@users.noreply.github.com>
2024-02-21 23:31:47 +0100

    feat: :sparkles: dae suported

--- anomalyDetection/src/application/algorithms/dae/DaeExecutor.py
+++ anomalyDetection/src/application/algorithms/dae/DaeExecutor.py
@@ -1,69 +1,55 @@
 import os
-from dataclasses import asdict
+from random import randint
+from time import time
 
-import pandas as pd
 from application.algorithms.dae.DaeData import DaeData
+from application.services.AlgorithmDataProcesor import AlgorithmDataProcesor
 from application.services.AlgorithmManager import AlgorithmManager
+from application.services.FileSystemService import FileSystemService
 from application.services.TimeEvalWrapper import TimeEvalParameters, TimeEvalWrapper
-from application.utils.datapaths import (
-    DATA_PATH_DOCKER,
-    DATA_PATH_HOST,
-    RESULTS_PATH_DOCKER,
-    RESULTS_PATH_HOST,
-)
 from domain.interfaces.AlgorithmExecutor import AlgorithmExecutor
 from domain.interfaces.EvaluationRepository import EvaluationRepository
-from domain.services.metrics import performance_metrics
+from domain.models.AlgorithmEvaluationMetrics import AlgorithmEvaluationMetrics
+from inject import Inject
 
 
 @AlgorithmManager.executor_for(DaeData)
+@Inject
 class DaeExecutor(AlgorithmExecutor):
-    def __init__(self, repository: EvaluationRepository, time_eval: TimeEvalWrapper):
-        self.time_eval = time_eval
-        self.repositoy = repository
 
-    def execute(self, command: DaeData):
-        self.executeAlgorithm(command)
-        self.evaluate_performance(
-            command.data_file, os.path.join(RESULTS_PATH_DOCKER, "anomaly_scores.ts")
-        )
+    def __init__(
+        self,
+        algorithm_data_procesor: AlgorithmDataProcesor,
+        time_eval_wrapper: TimeEvalWrapper,
+        repository: EvaluationRepository,
+        file_system_service: FileSystemService,
+    ):
+        self.time_eval_wrapper = time_eval_wrapper
+        self.algorithm_data_procesor = algorithm_data_procesor
+        self.repository = repository
+        self.file_system_service = file_system_service
 
-    def executeAlgorithm(self, command: DaeData):
-        param_dict = asdict(command)
-        del param_dict["data_file"]
+    def execute(self, data: DaeData):
+        output_file_name = data.__class__.__name__.replace("Data", "")
+        output_file_name = f"{output_file_name}_{randint(1000,9999)}_{os.path.basename(data.data_file)}"
 
-        time_eval_parameters = TimeEvalParameters(
-            name="dae",
-            execution_type="train",
-            data_input=command.data_file,
-            parameters=param_dict,
-        )
-
-        self.time_eval.execute(time_eval_parameters)
         time_eval_parameters = TimeEvalParameters(
             name="dae",
             execution_type="execute",
-            data_input=command.data_file,
-            parameters=param_dict,
+            model_output=data.model_name,
+            data_input=data.data_file,
+            data_output=output_file_name,
         )
-        self.time_eval.execute(time_eval_parameters)
 
-    def evaluate_performance(self, original_file, result_file):
-        file = os.path.join(DATA_PATH_DOCKER, original_file)
-        result_file = os.path.join(RESULTS_PATH_DOCKER, result_file)
-        anomaly_scores = pd.read_csv(result_file)
-        original_data = pd.read_csv(file)
+        t0 = time()
+        self.time_eval_wrapper.execute(time_eval_parameters)
+        t1 = time()
 
-        threshold = 0.5
-        anomaly_scores["binarized_scores"] = [
-            1 if x > threshold else 0 for x in anomaly_scores.iloc[:, 0]
-        ]
+        executionTime = round(t1 - t0, ndigits=4)
 
-        is_anomaly = original_data["is_anomaly"]
-        is_anomaly = is_anomaly[1:]
+        processed_data = self.file_system_service.read_resultsFrom(output_file_name)
 
-        metrics = performance_metrics(is_anomaly, anomaly_scores["binarized_scores"])
-
-        print(metrics)
-
-        return metrics
+        algorithm_evaluation_metrics: AlgorithmEvaluationMetrics = (
+            self.algorithm_data_procesor.process(data, processed_data, executionTime)
+        )
+        self.repository.save(algorithm_evaluation_metrics, data.report_file)
